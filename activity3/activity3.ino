668231d315fdc851af1cb069b85875f4b365a6b9
Mauricio Buschinelli <mauricio.buschinelli@johnabbott.qc.ca>
2021-07-14 21:39:25 -0400

    forgot to add sketch file

--- activity3/activity3.ino
+++ activity3/activity3.ino
@@ -6,7 +6,6 @@ float humidity = 0;
 // float pressure = 0; // pressure sensor broken
 int light = 0;
 int r,g,b;
- 
 // gyroscope
 float Gx, Gy, Gz;
 //accelerometer
@@ -14,6 +13,10 @@ float Ax, Ay, Az;
 
 //track how many measurements have been made
 int counter = 0;
+
+// Keeping track of SD card;
+bool sdEnable = true;
+long sdTries = 0;
 
 // file object
 File dataFile;
@@ -32,21 +35,82 @@ void setup() {
   }
 
   // init SD card
-  if (!SD.begin(SD_CS)) {
+  while (!SD.begin(SD_CS)) {
     Serial.println("Failed to initialize SD card!");
-    while (1);
+    delay(10);
+    sdTries++;
+    if(sdTries > 5){
+      sdEnable = false;
+      Serial.println("Bypassing SD card. Writting to serial only.");
+      break;
+    }
   }
-  dataFile = SD.open("log-0000.csv", FILE_WRITE);
-  Serial.println("SD file initialized");
-  // dataFile.println("temperature, humidity, pressure, light, Ax, Ay, Az, Gx, Gy, Gz");  // pressure sensor broken
-  dataFile.println("temperature, humidity, light, Ax, Ay, Az, Gx, Gy, Gz");
-  dataFile.close();
+  if(sdEnable){
+    dataFile = SD.open("log-0000.csv", FILE_WRITE);
+    Serial.println("SD file initialized");
+    // dataFile.println("temperature, humidity, pressure, light, Ax, Ay, Az, Gx, Gy, Gz");  // pressure sensor broken
+    dataFile.println("temperature, humidity, light, Ax, Ay, Az, Gx, Gy, Gz");
+    dataFile.close();
+  }
 }
 
 void loop() {
-  dataFile = SD.open("log-0000.csv", FILE_WRITE);
-  delay(100);
- 
-  temperature = 
+  if(sdEnable){
+    dataFile = SD.open("log-0000.csv", FILE_WRITE);
+    delay(100);
+  }
 
+  temperature = carrier.Env.readTemperature();
+  humidity = carrier.Env.readHumidity();
+
+  while(!carrier.Light.colorAvailable()){
+    delay(10);
+  }
+  carrier.Light.readColor(r,g,b,light);
+
+  // Read acceleration and direction
+  while(!carrier.IMUmodule.accelerationAvailable()){
+    delay(10);
+  }
+  carrier.IMUmodule.readAcceleration(Ax, Ay, Az);
+  while(!carrier.IMUmodule.gyroscopeAvailable()){
+    delay(10);
+  }
+  carrier.IMUmodule.readGyroscope(Gx, Gy, Gz);
+
+  counter++;
+
+  // Write data to serial and SD card
+  printComma(temperature);
+  printComma(humidity);
+  printComma(light);
+  printComma(Ax);
+  printComma(Ay);
+  printComma(Az);
+  printComma(Gx);
+  printComma(Gy);
+  printComma(Gz);
+  Serial.println("");
+  if(sdEnable){
+    dataFile.println("");
+    dataFile.close();
+  }
+  delay(1000);
+}
+
+void printComma(String text){
+  if(sdEnable){
+    dataFile.print(text);
+    dataFile.print(",");
+  }
+  Serial.print(text);
+  Serial.print(",");
+}
+
+void printComma(float value){
+  printComma(String(value));
+}
+
+void printComma(int value){
+  printComma(String(value));
 }
